d16726ce82bf4162b609a3ee6e4bb84130f01afc
CramY2001 <szymek.kramarczyk@gmail.com>
2024-10-22 00:30:30 +0200

    Lab1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,42 @@
-class Wektor2D
-{
-    // Tutaj napisz implementacje klasy Wektor2D
-};
+
+#include <iostream>
+#include <math.h>
+    class Wektor2D
+    {
+    public:
+        Wektor2D() { x=0.;y=0.;}
+        Wektor2D(double vx, double vy)
+        {
+            x=vx;
+            y=vy;
+            //std::cout<<x<<'\n';
+            //std::cout<<y<<'\n';
+        }
+
+        void setX (double vx) {x=vx;}
+        void setY (double vy) {y=vy;}
+        double getX () {return x;}
+        double getY () {return y;}
+    private:
+        double x;
+        double y;
+    };
+
+    Wektor2D operator+(Wektor2D v1,Wektor2D v2)
+    {
+        double x1t=v1.getX();
+        double y1t=v1.getY();
+        double x2t=v2.getX();
+        double y2t=v2.getY();
+        return Wektor2D{x1t+x2t,y1t+y2t};
+    }
+
+    double operator*(Wektor2D v1,Wektor2D v2)
+    {
+        double x1t=v1.getX();
+        double y1t=v1.getY();
+        double x2t=v2.getX();
+        double y2t=v2.getY();
+        double prod=x1t*x2t+y1t*y2t;
+        return prod;
+    };
